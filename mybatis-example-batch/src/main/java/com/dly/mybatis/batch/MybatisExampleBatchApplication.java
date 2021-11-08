d83875f0f5f9b62bfffc5b656258f70297ec2f16
duliao <duliao116@163.com>
2021-11-08 16:54:02 +0800

    first commit

---
+++ mybatis-example-batch/src/main/java/com/dly/mybatis/batch/MybatisExampleBatchApplication.java
@@ -0,0 +1,13 @@
+package com.dly.mybatis.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisExampleBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisExampleBatchApplication.class, args);
+    }
+
+}
---
+++ mybatis-example-batch/src/test/java/com/dly/mybatis/batch/MybatisExampleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dly.mybatis.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisExampleBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
